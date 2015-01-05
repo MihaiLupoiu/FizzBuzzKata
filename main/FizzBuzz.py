3b53974a673791444960698aa0fa0b37cfa95147
Mihaita Alexandru Lupoiu <myhay.360@gmail.com>
2015-01-05 16:13:22 +0100

    Resolución KataFizzBuzz. Enunciado: http://codingdojo.org/cgi-bin/index.pl?KataFizzBuzz

--- main/FizzBuzz.py
+++ main/FizzBuzz.py
@@ -4,7 +4,8 @@ Created on 1/1/2015
 @author: myhay
 '''
 
-def FizzBuzz(start, fin):
+#Implementacion del fizzBuzz solo si es divisible por 3, 5 o 15 (Fizz, Buzz o FizzBUuzz)
+def FizzBuzzStage1(start, fin):
     result = []
     for i in range(start,fin+1):
         if (i%15  == 0):
@@ -16,3 +17,23 @@ def FizzBuzz(start, fin):
         else:
             result.append(i)
     return result
+
+#Implementacion del fizzBuzz que si es divisible por 3, 5 o 15 sustituye su valor por Fizz, Buzz o FizzBuzz respectivamente.
+#Ademas si el numero contiene el valor 3 aparece Fizz y si el numero contiene 5 aparece Buzz.
+#La implmentacion se utilizo tal cual se explica en el ejemplo. 
+#Eso significa que si aparece el numero 53 por ejemplo, el resultado sera Fizz y no Buzz, dado que la cadena contiene un 3 y este valor se comprueba antes que el 5.
+def FizzBuzzStage2(start, fin):
+    result = []
+    for i in range(start,fin+1):
+        if (i%15  == 0):
+            result.append("FizzBuzz")
+        elif  (i%3 == 0) or ("3" in str(i)):
+            result.append("Fizz")
+        elif  (i%5 == 0) or ("5" in str(i)):
+            result.append("Buzz")
+        else:
+            result.append(i)
+    return result
+
+print FizzBuzzStage1(1, 100);
+print FizzBuzzStage2(1, 100);
--- test/test.py
+++ test/test.py
@@ -4,28 +4,51 @@ Created on 2/1/2015
 @author: myhay
 '''
 import unittest
-from main.FizzBuzz import FizzBuzz
 
-resultValue = [1, 2, 'Fizz', 4, 'Buzz', 'Fizz', 7, 8, 'Fizz', 'Buzz', 11, 'Fizz', 13, 14, 'FizzBuzz', 16, 17, 'Fizz', 19, 'Buzz', 'Fizz', 22, 23, 'Fizz', 'Buzz', 26, 'Fizz', 28, 29, 'FizzBuzz' ]
+from main.FizzBuzz import FizzBuzzStage1
+from main.FizzBuzz import FizzBuzzStage2
+
+resultValueStage1 = [1, 2, 'Fizz', 4, 'Buzz', 'Fizz', 7, 8, 'Fizz', 'Buzz', 11, 'Fizz', 13, 14, 'FizzBuzz', 16, 17, 'Fizz', 19, 'Buzz', 'Fizz', 22, 23, 'Fizz', 'Buzz', 26, 'Fizz', 28, 29, 'FizzBuzz' ]
+resultValueStage2 = [1, 2, 'Fizz', 4, 'Buzz', 'Fizz', 7, 8, 'Fizz', 'Buzz', 11, 'Fizz', 'Fizz', 14, 'FizzBuzz', 16, 17, 'Fizz', 19, 'Buzz', 'Fizz', 22, 'Fizz', 'Fizz', 'Buzz', 26, 'Fizz', 28, 29, 'FizzBuzz', 'Fizz', 'Fizz', 'Fizz', 'Fizz', 'Fizz', 'Fizz', 'Fizz', 'Fizz', 'Fizz', 'Buzz']
 
 class Test(unittest.TestCase):
 
-    def test10FizzBuzz(self):
-        result = FizzBuzz(1, 10);
-        self.assertListEqual(result, resultValue[0:10], "10 check faild")
+# Pruebaas de la primera parte
+
+    def test10FizzBuzzStage1(self):
+        result = FizzBuzzStage1(1, 10);
+        self.assertListEqual(result, resultValueStage1[0:10], "10 check faild")
 
 
-    def test15FizzBuzz(self):
-        result = FizzBuzz(1, 15);
-        self.assertListEqual(result, resultValue[0:15], "15 check faild")
+    def test15FizzBuzzStage1(self):
+        result = FizzBuzzStage1(1, 15);
+        self.assertListEqual(result, resultValueStage1[0:15], "15 check faild")
         
-    def test30FizzBuzz(self):
-        result = FizzBuzz(1, 30);
-        self.assertListEqual(result, resultValue, "30 check faild")
+    def test30FizzBuzzStage1(self):
+        result = FizzBuzzStage1(1, 30);
+        self.assertListEqual(result, resultValueStage1, "30 check faild")
         
-    def test100FizzBuzz(self):
-        result = FizzBuzz(1, 100);
+    def test100FizzBuzzStage1(self):
+        result = FizzBuzzStage1(1, 100);
         print result
 
+# Pruebaas de la segunda parte
+
+    def test10FizzBuzzStage2(self):
+        result = FizzBuzzStage2(1, 10);
+        self.assertListEqual(result, resultValueStage2[0:10], "10 check faild")
+
+    def test15FizzBuzzStage2(self):
+        result = FizzBuzzStage2(1, 15);
+        self.assertListEqual(result, resultValueStage2[0:15], "15 check faild")
+        
+    def test30FizzBuzzStage2(self):
+        result = FizzBuzzStage2(1, 40);
+        self.assertListEqual(result, resultValueStage2, "40 check faild")
+
+    def test100FizzBuzzStage2(self):
+        result = FizzBuzzStage2(1, 100);
+        print result
+        
 if __name__ == '__main__':
     unittest.main()
