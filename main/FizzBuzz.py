28668908b93dda192a69af8475309b4c9cd10d03
Mihaita Alexandru Lupoiu <myhay.360@gmail.com>
2015-01-10 02:25:47 +0100

    Mejora del Fizz Buzz. Pendiente Revisar DRY en tests.

--- main/FizzBuzz.py
+++ main/FizzBuzz.py
@@ -4,36 +4,39 @@ Created on 1/1/2015
 @author: myhay
 '''
 
-#Implementacion del fizzBuzz solo si es divisible por 3, 5 o 15 (Fizz, Buzz o FizzBUuzz)
-def FizzBuzzStage1(start, fin):
+def isFizz(number):
+    if  (number%3 == 0) or ("3" in str(number)):
+            return True
+    return False
+    
+def isBuzz(number):
+    if  (number%5 == 0) or ("5" in str(number)):
+            return True
+    return False
+        
+def isFizzBuzz(number):
+    if (number %15  == 0):
+        return True
+    return False
+
+def fizzBuzz(number):
+    if (isFizzBuzz(number)):
+        return "FizzBuzz"
+    elif  (isFizz(number)):
+        return "Fizz"
+    elif  (isBuzz(number)):
+        return "Buzz"
+    else:
+        return number
+
+def FizzBuzzRange(lista):
     result = []
-    for i in range(start,fin+1):
-        if (i%15  == 0):
-            result.append("FizzBuzz")
-        elif (i%3 == 0):
-            result.append("Fizz")
-        elif (i%5 == 0):
-            result.append("Buzz")
-        else:
-            result.append(i)
+    for i in lista:
+            result.append(fizzBuzz(i))
     return result
 
-#Implementacion del fizzBuzz que si es divisible por 3, 5 o 15 sustituye su valor por Fizz, Buzz o FizzBuzz respectivamente.
-#Ademas si el numero contiene el valor 3 aparece Fizz y si el numero contiene 5 aparece Buzz.
-#La implmentacion se utilizo tal cual se explica en el ejemplo. 
-#Eso significa que si aparece el numero 53 por ejemplo, el resultado sera Fizz y no Buzz, dado que la cadena contiene un 3 y este valor se comprueba antes que el 5.
-def FizzBuzzStage2(start, fin):
+def FizzBuzzIncrementalRange(start, fin):
     result = []
     for i in range(start,fin+1):
-        if (i%15  == 0):
-            result.append("FizzBuzz")
-        elif  (i%3 == 0) or ("3" in str(i)):
-            result.append("Fizz")
-        elif  (i%5 == 0) or ("5" in str(i)):
-            result.append("Buzz")
-        else:
-            result.append(i)
+            result.append(fizzBuzz(i))
     return result
-
-print FizzBuzzStage1(1, 100);
-print FizzBuzzStage2(1, 100);
--- test/test.py
+++ test/test.py
@@ -5,49 +5,50 @@ Created on 2/1/2015
 '''
 import unittest
 
-from main.FizzBuzz import FizzBuzzStage1
-from main.FizzBuzz import FizzBuzzStage2
+from main.FizzBuzz import FizzBuzzIncrementalRange
+from main.FizzBuzz import fizzBuzz
+from main.FizzBuzz import FizzBuzzRange
 
-resultValueStage1 = [1, 2, 'Fizz', 4, 'Buzz', 'Fizz', 7, 8, 'Fizz', 'Buzz', 11, 'Fizz', 13, 14, 'FizzBuzz', 16, 17, 'Fizz', 19, 'Buzz', 'Fizz', 22, 23, 'Fizz', 'Buzz', 26, 'Fizz', 28, 29, 'FizzBuzz' ]
-resultValueStage2 = [1, 2, 'Fizz', 4, 'Buzz', 'Fizz', 7, 8, 'Fizz', 'Buzz', 11, 'Fizz', 'Fizz', 14, 'FizzBuzz', 16, 17, 'Fizz', 19, 'Buzz', 'Fizz', 22, 'Fizz', 'Fizz', 'Buzz', 26, 'Fizz', 28, 29, 'FizzBuzz', 'Fizz', 'Fizz', 'Fizz', 'Fizz', 'Fizz', 'Fizz', 'Fizz', 'Fizz', 'Fizz', 'Buzz']
+
+resultValue = [1, 2, 'Fizz', 4, 'Buzz', 'Fizz', 7, 8, 'Fizz', 'Buzz', 11, 'Fizz', 'Fizz', 14, 'FizzBuzz', 16, 17, 'Fizz', 19, 'Buzz', 'Fizz', 22, 'Fizz', 'Fizz', 'Buzz', 26, 'Fizz', 28, 29, 'FizzBuzz', 'Fizz', 'Fizz', 'Fizz', 'Fizz', 'Fizz', 'Fizz', 'Fizz', 'Fizz', 'Fizz', 'Buzz']
 
 class Test(unittest.TestCase):
 
-# Pruebaas de la primera parte
+    def test_3_OnlyFizzBuzz(self):
+        self.assertEqual(fizzBuzz(3), "Fizz", "3 check faild")
 
-    def test10FizzBuzzStage1(self):
-        result = FizzBuzzStage1(1, 10);
-        self.assertListEqual(result, resultValueStage1[0:10], "10 check faild")
-
-
-    def test15FizzBuzzStage1(self):
-        result = FizzBuzzStage1(1, 15);
-        self.assertListEqual(result, resultValueStage1[0:15], "15 check faild")
+    def test_5_OnlyFizzBuzz(self):
+        self.assertEqual(fizzBuzz(5), "Buzz", "5 check faild")
         
-    def test30FizzBuzzStage1(self):
-        result = FizzBuzzStage1(1, 30);
-        self.assertListEqual(result, resultValueStage1, "30 check faild")
+    def test_15_OnlyFizzBuzz(self):
+        self.assertEqual(fizzBuzz(15), "FizzBuzz", "15 check faild")
+    
+    def test_31_OnlyFizzBuzz(self):
+        self.assertEqual(fizzBuzz(31), "Fizz", "31 check faild")
+    
+    def test_51_OnlyFizzBuzz(self):
+        self.assertEqual(fizzBuzz(52), "Buzz", "52 check faild")
+    
+    def test_44_OnlyFizzBuzz(self):
+        self.assertEqual(fizzBuzz(44), 44, "44 check faild")
+    
+    def test_10_FizzBuzz(self):
+        result = FizzBuzzIncrementalRange(1, 10);
+        self.assertListEqual(result, resultValue[0:10], "10 check faild")
+
+    def test_15_FizzBuzz(self):
+        result = FizzBuzzIncrementalRange(1, 15);
+        self.assertListEqual(result, resultValue[0:15], "15 check faild")
         
-    def test100FizzBuzzStage1(self):
-        result = FizzBuzzStage1(1, 100);
-        print result
+    def test_30_FizzBuzz(self):
+        result = FizzBuzzIncrementalRange(1, 40);
+        self.assertListEqual(result, resultValue, "40 check faild")
+    
+    def test_vector_FizzBuzz(self):
+        self.assertListEqual(FizzBuzzRange([1, 6, 4, 58, 15]), [1, 'Fizz', 4,'Buzz', 'FizzBuzz'], "list check faild")
 
-# Pruebaas de la segunda parte
-
-    def test10FizzBuzzStage2(self):
-        result = FizzBuzzStage2(1, 10);
-        self.assertListEqual(result, resultValueStage2[0:10], "10 check faild")
-
-    def test15FizzBuzzStage2(self):
-        result = FizzBuzzStage2(1, 15);
-        self.assertListEqual(result, resultValueStage2[0:15], "15 check faild")
-        
-    def test30FizzBuzzStage2(self):
-        result = FizzBuzzStage2(1, 40);
-        self.assertListEqual(result, resultValueStage2, "40 check faild")
-
-    def test100FizzBuzzStage2(self):
-        result = FizzBuzzStage2(1, 100);
+    def test_100_FizzBuzz(self):
+        result = FizzBuzzIncrementalRange(1, 100);
         print result
         
 if __name__ == '__main__':
